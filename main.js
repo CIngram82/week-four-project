c8b54e0a8db69ad969ba3d9e00205489b64a5f0a
Christopher Ingram <cingram82@gmail.com>
2017-08-09 15:06:27 -0400

    search working basic css done.

--- main.js
+++ main.js
@@ -7,3 +7,34 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+function searching(){
+fetch(`https://itunes.apple.com/search?term=${searchTermLocation.value}`)
+  .then(convertFromJson)
+  .then(builtTheArray)
+}
+function convertFromJson(results){
+  return results.json()
+}
+
+let resultsArea= document.querySelector(".resultsArea");
+let searchTermLocation = document.querySelector("#searchField")
+let arrayOfSongs =[];
+let searchButt = document.querySelector("#submitButton")
+searchButt.addEventListener("click", searching);
+
+function  builtTheArray(data){
+resultsArea.innerHTML = "";
+arrayOfSongs = data;
+console.log(arrayOfSongs);
+for (var i = 0; i < arrayOfSongs.results.length; i++) {
+  resultsArea.innerHTML +=`
+  <div class="song" id="song${i}">
+  <img src="${arrayOfSongs.results[i].artworkUrl100}">
+  <a href="${arrayOfSongs.results[i].trackViewUrl}"><h3>${arrayOfSongs.results[i].trackName}</h3></a>
+  <p>${arrayOfSongs.results[i].artistName}</p>
+  <button type="button" name="Button${i}" id="Button${i}">Play a sample!</button>
+</div>
+  `
+  
+}
+}
