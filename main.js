1aa6b7e82ee7790d50392859e72c0f4edba267f3
Christopher Ingram <cingram82@gmail.com>
2017-08-14 08:22:44 -0400

    work on tabs started

--- main.js
+++ main.js
@@ -1,48 +1,46 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
-
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
-function searching() {
-  fetch(`https://itunes.apple.com/search?term=${searchTermLocation.value}`)
-    .then(convertFromJson)
-    .then(builtTheArray)
-}
-
-function convertFromJson(results) {
-  return results.json()
-}
-
-let resultsArea = document.querySelector(".resultsArea");
+let resultsArea = document.querySelector("#resultsArea");
 let searchTermLocation = document.querySelector("#searchField")
 let arrayOfSongs = [];
 let searchButt = document.querySelector("#submitButton")
 searchButt.addEventListener("click", searching);
 let songLocation = document.querySelector("#music-player");
 let videoLocation = document.querySelector("#video-player");
+let tabArea = document.querySelector(".tabs");
+let bodyArea = document.querySelector("body");
+tabArea.addEventListener("click", tabChange);
 
-document.querySelector(".resultsArea").addEventListener("click", function(bananapants) {
+function convertFromJson(results) {
+  return results.json()
+}
+
+
+function searching() {
+  fetch(`https://itunes.apple.com/search?term=${searchTermLocation.value}`)
+    .then(convertFromJson)
+    .then(builtTheArray)
+}
+function tabChange(tabClicked){
+  console.log(tabClicked.target.id);
+  if (tabClicked.target.id === "songTab") {
+    bodyArea.className = "songs";
+  }else if (tabClicked.target.id === "videoTab") {
+      bodyArea.className = "videos";
+  }else if (tabClicked.target.id === "podcastTab") {
+    bodyArea.className = "podcasts";
+  }
+}
+
+resultsArea.addEventListener("click", function(bananapants) {
   let lastThree = bananapants.target.value.slice(-3);
-  console.log(lastThree);
 
   if (lastThree === "m4v") {
     videoLocation.src = bananapants.target.value;
     videoLocation.autoplay = "true";
-    songLocation.style.display = "none"
-    videoLocation.style.display = "block"
-
   } else if (lastThree === "m4a") {
     songLocation.src = bananapants.target.value;
     songLocation.autoplay = "true";
-    videoLocation.style.display = "none"
-    songLocation.style.display = "block"
   }
 });
-
 
 function builtTheArray(data) {
   resultsArea.innerHTML = "";
@@ -51,12 +49,11 @@ function builtTheArray(data) {
   for (var i = 0; i < arrayOfSongs.results.length; i++) {
     resultsArea.innerHTML += `
   <div class="song" id="song${i}">
-  <img src="${arrayOfSongs.results[i].artworkUrl100}">
-  <a href="${arrayOfSongs.results[i].trackViewUrl}"><h3>${arrayOfSongs.results[i].trackName}</h3></a>
-  <p>${arrayOfSongs.results[i].artistName}</p>
-  <button type="button" name="Button${i}" id="Button${i}" value=${arrayOfSongs.results[i].previewUrl}>Play a sample!</button>
-</div>
+    <img src="${arrayOfSongs.results[i].artworkUrl100}">
+    <a href="${arrayOfSongs.results[i].trackViewUrl}"><h3>${arrayOfSongs.results[i].trackName}</h3></a>
+    <p>${arrayOfSongs.results[i].artistName}</p>
+    <button type="button" name="Button${i}" id="Button${i}" value=${arrayOfSongs.results[i].previewUrl}>Play a sample!</button>
+  </div>
   `
-
-  }
+}
 }
